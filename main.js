a3abe2b5afa31a66374c28f2fec845bd6b5a7f04
Ben Turner <benpturner1@gmail.com>
2024-07-14 20:01:54 +1000

    add .gitignore to exlude node modules

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1800,
+        height: 200,
+        webPreferences: {
+            nodeIntegration: true  
+        },
+        
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
